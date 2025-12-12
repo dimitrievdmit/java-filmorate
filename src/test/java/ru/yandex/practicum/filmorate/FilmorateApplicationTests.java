a42a74d9d214d9e4825a09c473d2a9824afcf02a
dimitrievdd <dimitrievdmit@yandex.ru>
2025-12-12 23:55:14 +0300

    Спринт 10. fix: Исправлен отступ в FilmorateApplicationTests в соответствии сheckStyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FilmorateApplicationTests {
 
-	@SuppressWarnings("EmptyMethod")
+    @SuppressWarnings("EmptyMethod")
     @Test
-	void contextLoads() {
-	}
+    void contextLoads() {
+    }
 
 }
